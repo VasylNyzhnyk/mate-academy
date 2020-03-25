3d08151eebbf7c8aae5c06e7b7fd3cf549e061f6
My <vasylnv19@gmail.com>
2020-03-25 17:18:28 +0200

    add lopp

--- src/main/java/TestConnection.java
+++ src/main/java/TestConnection.java
@@ -2,5 +2,9 @@ public class TestConnection {
     public static void main(String[] args) {
         System.out.println("Hello mates");
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello mates");
+        }
+
     }
 }
